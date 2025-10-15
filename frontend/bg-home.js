c8e63ed46ce98dfd867e3842ecda2b32c7fa9824
MIGUEL ANGEL CHILLON VAZQUEZ <zc01331@tc2.tech>
2025-10-15 09:05:51 +0200

    Commit11

---
+++ frontend/bg-home.js
@@ -0,0 +1,5 @@
+// bg-home.js — añade la clase 'home' al <body> (para activar el fondo)
+// Inclúyelo SOLO en index.html
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.classList.add('home');
+});
