e99b433566eb6d2976fa548e01c5133cf0a9dc13
MIGUEL ANGEL CHILLON VAZQUEZ <zc01331@tc2.tech>
2025-10-15 11:11:25 +0200

    Commit15

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import fetch from 'node-fetch';
 import { createClient } from '@supabase/supabase-js';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
